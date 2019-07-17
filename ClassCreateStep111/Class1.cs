364842168e080294f729657c20bfeffca94075f8
Cnix24 <calvinattl24@gmail.com>
2019-07-17 14:00:08 -0600

    Update

---
+++ ClassCreateStep111/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCreateStep111
+{
+    public class Mathing
+    {
+        public int inputValue { get; set; }
+        public int Multiplier { get; set; }
+        public int Adder { get; set; }
+        public int Subtractor { get; set; }
+        public Mathing(int inputValue)
+            {
+            Multiplier = inputValue * 10;
+            Adder = inputValue + 23;
+            Subtractor = inputValue - 9;
+            }
+    }
+}
---
+++ ClassCreateStep111/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCreateStep111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputValue;
+            string userInput;
+
+            Console.WriteLine("Please provide a number for math results: ");
+            userInput = Console.ReadLine();
+            inputValue = Convert.ToInt32(userInput);
+
+            Mathing multiply = new Mathing(inputValue);
+            
+
+            Console.WriteLine("Your number " + inputValue + " times 10 is " + multiply.Multiplier);
+            Console.WriteLine("Your number " + inputValue + " plus 23 is " + multiply.Adder);
+            Console.WriteLine("Your number " + inputValue + " minus 9 is " + multiply.Subtractor);
+        }
+    }
+}
